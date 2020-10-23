4bfae13566a63b961acabfddf88030cc4d6edd71
Natalya Plyusnina <n.plyusnina@xsolla.com>
2020-10-23 14:37:03 +0500

    linter fix

--- bin/games/brain-gcd.js
+++ bin/games/brain-gcd.js
@@ -10,8 +10,8 @@ for (let i = 0; i < questionLimit; i += 1) {
   const num2 = Math.floor(Math.random() * 100) + 1
   questions[i] = `${num1} ${num2}`
   const gcd = (num1, num2) => {
-    while (num2 !== 0) num2 = num1 % (num1 = num2);
-    return num1;
+    while (num2 !== 0) num2 = num1 % (num1 = num2)
+    return num1
   }
   answers[i] = gcd(num1, num2)
 }
