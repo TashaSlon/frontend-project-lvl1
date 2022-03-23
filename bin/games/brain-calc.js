4a40a0b64c095db80599151001024a50bab0223a
plyusnina <plyusnina@live.ru>
2022-03-23 10:17:53 +0500

    Linter fixes

--- bin/games/brain-calc.js
+++ bin/games/brain-calc.js
@@ -1,30 +1,30 @@
-import common from '../../src/index.js'
+import common from '../../src/index.js';
 
 const createQuestion = () => {
-	const operators = ['+', '-', '*'];
-	const num1 = Math.floor(Math.random() * 100);
-	const num2 = Math.floor(Math.random() * 100);
-	const operator = Math.floor(Math.random() * 3);
-	const expression = `${num1} ${operators[operator]} ${num2}`;
-	let result = 0;
-	switch (operator) {
-		case 0:
-			result = num1 + num2;
-			break;
-		case 1:
-			result = num1 - num2;
-			break;
-		case 2:
-			result = num1 * num2;
-			break;
-		default:
-			result = num1 + num2;
-	}
-	return [expression, String(result)];
+  const operators = ['+', '-', '*'];
+  const num1 = Math.floor(Math.random() * 100);
+  const num2 = Math.floor(Math.random() * 100);
+  const operator = Math.floor(Math.random() * 3);
+  const expression = `${num1} ${operators[operator]} ${num2}`;
+  let result = 0;
+  switch (operator) {
+    case 0:
+      result = num1 + num2;
+      break;
+    case 1:
+      result = num1 - num2;
+      break;
+    case 2:
+      result = num1 * num2;
+      break;
+    default:
+      result = num1 + num2;
+  }
+  return [expression, String(result)];
 };
 
 const questions = ['What is the result of the expression?'];
 for (let i = 0; i < 3; i += 1) {
-	questions.push(createQuestion());
+  questions.push(createQuestion());
 }
 common(questions);
--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -1,16 +1,14 @@
-#!/usr/bin/env node
-
-import common from "../../src/index.js";
+import common from '../../src/index.js';
 
 const createQuestion = () => {
-	const expression = Math.floor(Math.random() * 100);
-	let result = (expression % 2 === 0) ? 'yes' : 'no';
+  const expression = Math.floor(Math.random() * 100);
+  const result = (expression % 2 === 0) ? 'yes' : 'no';
 
-	return [expression, result];
+  return [expression, result];
 };
 
 const questions = ['Answer "yes" if the number is even, otherwise answer "no".'];
 for (let i = 0; i < 3; i += 1) {
-	questions.push(createQuestion());
+  questions.push(createQuestion());
 }
 common(questions);
